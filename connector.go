f83272f79c09f16854dd43ced63e139457f6d0a8
shtrikulis <ilya.shtrikul@vseinstrumenti.ru>
2020-12-08 13:00:26 +0300

    fix config

--- connector.go
+++ connector.go
@@ -6,7 +6,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 	"go.mongodb.org/mongo-driver/mongo/readpref"
 	"go.mongodb.org/mongo-driver/tag"
-	"go.mongodb.org/mongo-driver/x/mongo/driver/auth"
 )
 
 // NewConnector returns a new database connector for the application.
@@ -15,50 +14,54 @@ func NewConnector(config Config) (*Client, error) {
 
 	if config.URI != "" {
 		opts = opts.ApplyURI(config.URI)
-	} else {
-		opts = opts.SetHosts(config.Hosts).
-			SetAuth(options.Credential{
-				AuthMechanism: auth.PLAIN,
-				AuthSource:    config.Name,
-				Username:      config.User,
-				Password:      config.Pass,
-			})
+	}
 
-		if config.ReplicaSet != nil {
-			opts.ReplicaSet = config.ReplicaSet
+	if len(config.Hosts) > 0 {
+		opts = opts.SetHosts(config.Hosts)
+	}
+
+	if config.User != "" {
+		opts = opts.SetAuth(options.Credential{
+			AuthSource: config.Name,
+			Username:   config.User,
+			Password:   config.Pass,
+		})
+	}
+
+	if config.ReplicaSet != nil {
+		opts.ReplicaSet = config.ReplicaSet
+	}
+
+	if config.ReadPreference != nil {
+		var rpOpts []readpref.Option
+
+		if config.ReadPreference.MaxStaleness != nil {
+			rpOpts = append(rpOpts, readpref.WithMaxStaleness(*config.ReadPreference.MaxStaleness))
 		}
 
-		if config.ReadPreference != nil {
-			var rpOpts []readpref.Option
-
-			if config.ReadPreference.MaxStaleness != nil {
-				rpOpts = append(rpOpts, readpref.WithMaxStaleness(*config.ReadPreference.MaxStaleness))
-			}
-
-			if len(config.ReadPreference.Tags) > 0 {
-				var tagSet tag.Set
-				for _, t := range config.ReadPreference.Tags {
-					if t == "" {
-						tagSet = append(tagSet, tag.Tag{})
-						break
-					}
-					kv := strings.Split(t, ":")
-					if len(kv) != 2 {
-						continue
-					}
-					tagSet = append(tagSet, tag.Tag{Name: kv[0], Value: kv[1]})
+		if len(config.ReadPreference.Tags) > 0 {
+			var tagSet tag.Set
+			for _, t := range config.ReadPreference.Tags {
+				if t == "" {
+					tagSet = append(tagSet, tag.Tag{})
+					break
 				}
-
-				if len(tagSet) > 0 {
-					rpOpts = append(rpOpts, readpref.WithTagSets(tagSet))
+				kv := strings.Split(t, ":")
+				if len(kv) != 2 {
+					continue
 				}
+				tagSet = append(tagSet, tag.Tag{Name: kv[0], Value: kv[1]})
 			}
 
-			var err error
-			opts.ReadPreference, err = readpref.New(config.ReadPreference.Mode, rpOpts...)
-			if err != nil {
-				return nil, err
+			if len(tagSet) > 0 {
+				rpOpts = append(rpOpts, readpref.WithTagSets(tagSet))
 			}
+		}
+
+		var err error
+		opts.ReadPreference, err = readpref.New(config.ReadPreference.Mode, rpOpts...)
+		if err != nil {
+			return nil, err
 		}
 	}
 
